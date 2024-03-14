7774e33ba5e5eb4b7886e3e5ec06374fe136cf05
Mohammadi Iram <miram@redhat.com>
2024-03-14 16:54:30 +0530

    add github action

--- sha_update.sh
+++ sha_update.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
 
-# Specify the repository URL
+
 REPO_URL="https://github.com/red-hat-data-services/kserve.git"
-# Specify the branch to checkout, for example, 'master'
-BRANCH_NAME="rhoai-2.8"
-# Specify the file name you're looking for
 FILE_PATH="kserve/config/overlays/odh/params.env"
 
-echo "$FILE_PATH"
-# Clone the repository
-# git clone -b $BRANCH_NAME $REPO_URL kserve
-# git clone --depth 1 -b "$BRANCH_NAME" "https://github.com/red-hat-data-services/kserve.git" "kserve"
-
-
-# Change directory to the cloned repository
-# cd kserve
-
-# Find the file within the repository. Adjust the path as needed.
-# This example uses 'find' to search the whole repository for the file.
-# If you know the exact directory, you can directly use 'cat' to print its content.
-if [ -f "$FILE_PATH" ]; then
-    echo "Found file: $FILE_PATH"
-    echo "Contents of $FILE_PATH:"
-   # cat "$FILE_PATH"
-else
-    echo "File not found: $FILE_PATH"
-fi
-
-# Optionally, cleanup by removing the cloned repository if no longer needed
-# cd ..
-# rm -rf kserve_repo
-
+function clone_repo() {
+  local BRANCH_NAME=$1
+  git clone --depth 1 -b "$BRANCH_NAME" "https://github.com/red-hat-data-services/kserve.git" "kserve"
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to access $repo"
+    return 1
+  fi
+}
+function get_latest_rhods_version() {
+  local rhods_version
+  rhods_version=$(git ls-remote --heads https://github.com/red-hat-data-services/rhods-operator | grep 'rhoai' | awk -F'/' '{print $NF}' | sort -V | tail -1)
+  echo "$rhods_version"
+}
 
 
 extract_names_with_sbom_extension() {
@@ -47,9 +33,11 @@ extract_names_with_sbom_extension() {
 
     if [ "$hash" = "$names_part" ]; then
         # Print in green if equal
+        echo "$tag"
         echo -e "\e[32m$hash\e[0m matches \e[32m$names_part\e[0m"
     else
         # Print in red if not equal
+        echo "$tag"
         echo -e "\e[31m$hash\e[0m does not match \e[31m$names_part\e[0m"
     fi
 
@@ -57,14 +45,16 @@ extract_names_with_sbom_extension() {
 
 main(){
 
+    rhods_version=$(get_latest_rhods_version)
+    echo "$rhods_version"
     # call function for clone
+
+    clone_repo $rhods_version
     # read from file and store the contetnt in input varial
 
-    local input='kserve-controller=quay.io/modh/kserve-controller@sha256:af19ec0f06a579f4f7778fef8bbf9c559c1814953593b1e64704b262c09d614a
-kserve-agent=quay.io/modh/kserve-agent@sha256:fa885ed04ea836d9ec2ae038a5d6721010566a2df6df3c615e4f3cefb14794d9
-kserve-router=quay.io/modh/kserve-router@sha256:3ce38cc18a92f35da98d371248a2d9d01b1d8f10ec94a6b94f8ec1922d436028'
-
-  #  local input="$1"
+    if [ -f "$FILE_PATH" ]; then
+    echo "File found: $FILE_PATH"
+    local input=$(<"$FILE_PATH")
 
     # Extract names before '=' using cut
     local names=$(echo "$input" | cut -d'=' -f1)
@@ -91,7 +81,10 @@ kserve-router=quay.io/modh/kserve-router@sha256:3ce38cc18a92f35da98d371248a2d9d0
        # echo "Name: $name"
        # echo "Hash: $hash"
     done <<< "$input"
-
+    rm -rf kserve
+    else
+    echo "File not found: $FILE_PATH"
+    fi
 
 
 }
